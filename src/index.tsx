c8e9e9e70a167d34a0998d39b510576f9195cd85
Duc Trung Le <leductrungxf@gmail.com>
2023-08-04 14:01:15 +0200

    Create widgets

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import {
 } from '@jupyterlab/application';
 import { IThemeManager } from '@jupyterlab/apputils';
 import { ReactWidget } from '@jupyterlab/ui-components';
+import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
+import { Kernel } from '@jupyterlab/services';
 import { TopbarElement } from './widget';
 import * as React from 'react';
 
@@ -14,17 +16,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'voila_topbar:plugin',
   description: 'A Voila extension',
   autoStart: true,
-  requires: [IThemeManager],
+  requires: [IThemeManager, IJupyterWidgetRegistry],
   activate: (app: JupyterFrontEnd, themeManager: IThemeManager) => {
     if (app.name !== 'Voila') {
       return;
     }
-    console.log(
-      'JupyterLab extension voila_topbar is activated!',
-      themeManager
-    );
+    const kernelConnection = (app as any)?.widgetManager?.kernel as
+      | Kernel.IKernelConnection
+      | undefined;
+
     const widget = ReactWidget.create(
-      <TopbarElement config={{}} themeManager={themeManager} />
+      <TopbarElement
+        config={{}}
+        themeManager={themeManager}
+        kernelConnection={kernelConnection}
+      />
     );
     widget.addClass('voila-topbar');
     widget.id = 'voila-topbar-element';
--- src/widget.tsx
+++ src/widget.tsx
@@ -1,5 +1,7 @@
 import { IThemeManager } from '@jupyterlab/apputils';
 import * as React from 'react';
+import { Kernel } from '@jupyterlab/services';
+import { ProgressCircle } from './kernelStatus';
 
 interface IProps {
   config: {
@@ -8,12 +10,14 @@ interface IProps {
     themeToggle?: boolean;
   };
   themeManager: IThemeManager;
+  kernelConnection?: Kernel.IKernelConnection;
 }
 
 export function TopbarElement(props: IProps): JSX.Element {
   const [themeOptions, setThemeOptions] = React.useState<string[]>([
     ...props.themeManager.themes
   ]);
+  const [kernelBusy, setkernelBusy] = React.useState<0 | 100>(100);
   React.useEffect(() => {
     const cb = (sender: IThemeManager, args: any) => {
       if (args.newValue.length > 0) {
@@ -22,15 +26,52 @@ export function TopbarElement(props: IProps): JSX.Element {
       setThemeOptions([...props.themeManager.themes]);
     };
     props.themeManager.themeChanged.connect(cb);
-    return () => void props.themeManager.themeChanged.disconnect(cb);
-  }, [props.themeManager]);
-  console.log('themeOptions', themeOptions);
+    const kernelCb = (sender: any, status: any) => {
+      const progress = status === 'busy' ? 0 : 100;
+      setkernelBusy(progress);
+    };
+    if (props.kernelConnection) {
+      props.kernelConnection.statusChanged.connect(kernelCb);
+    }
 
+    return () => {
+      props.themeManager.themeChanged.disconnect(cb);
+      if (props.kernelConnection) {
+        props.kernelConnection.statusChanged.disconnect(kernelCb);
+      }
+    };
+  }, [props.themeManager, props.kernelConnection]);
+
+  const onThemeChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const theme = e.currentTarget?.value;
+      if (theme) {
+        props.themeManager.setTheme(theme);
+      }
+    },
+    [props.themeManager]
+  );
+
+  const themeSelect = React.useMemo(() => {
+    return (
+      <select className="voila-topbar-theme" onChange={onThemeChange}>
+        {themeOptions.map(el => {
+          return (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          );
+        })}
+      </select>
+    );
+  }, [themeOptions, onThemeChange]);
   return (
     <React.Fragment>
       <div className="voila-topbar-title">
         {props.config.title ?? 'Voila Dashboard'}
       </div>
+      {themeSelect}
+      <ProgressCircle progress={kernelBusy} width={50} height={50} />
     </React.Fragment>
   );
 }
