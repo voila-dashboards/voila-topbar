4d87d1920c54688a4b83e6234ab768002c6dfb89
Duc Trung Le <leductrungxf@gmail.com>
2023-08-21 21:45:12 +0200

    Update color

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import {
 } from '@jupyterlab/application';
 import { IThemeManager } from '@jupyterlab/apputils';
 import { ReactWidget } from '@jupyterlab/ui-components';
-import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
 import { Kernel } from '@jupyterlab/services';
 import { ITopbarConfig, TopbarElement } from './widget';
 import * as React from 'react';
@@ -16,7 +15,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'voila_topbar:plugin',
   description: 'A Voila extension',
   autoStart: true,
-  requires: [IThemeManager, IJupyterWidgetRegistry],
+  requires: [IThemeManager],
   activate: (app: JupyterFrontEnd, themeManager: IThemeManager) => {
     if (app.name !== 'Voila') {
       return;
@@ -26,7 +25,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
       | undefined;
     const extensionConfig = JSON.parse(PageConfig.getOption('extensionConfig'));
     const config = extensionConfig['voila_topbar'] as ITopbarConfig | undefined;
-    console.log('config', config);
 
     const widget = ReactWidget.create(
       <TopbarElement
@@ -37,6 +35,10 @@ const plugin: JupyterFrontEndPlugin<void> = {
     );
     widget.id = 'voila-topbar-element';
     app.shell.add(widget, 'top');
+    if (widget.parent) {
+      widget.parent.node.style.boxShadow =
+        'rgba(0 0 0 / 20%) 0 2px 4px -1px, rgba(0 0 0 / 14%) 0 4px 5px 0, rgba(0 0 0 / 12%) 0 1px 10px 0';
+    }
   }
 };
 
